341100065997254d23c3bc712e77a9529bbca21a
Gabor Racz <gabor@granadilla.ai>
2019-07-07 18:01:37 +0200

    added input validation

--- src/script.js
+++ src/script.js
@@ -4,14 +4,26 @@ const CALLBTN = document.querySelector("#calculate");
 const PEOPLE = document.getElementById("peopleAtm");
 const TOTAL = document.getElementById("tip");
 
-function showText(){
-	const serviceFee = (BILLAMT.value * SERVICE_FEE.value);
-	const total = serviceFee + parseInt(BILLAMT.value);
-    
-	const totalEach = total / parseInt(PEOPLE.value);
-	console.log(totalEach);
-	TOTAL.innerText = totalEach;
+document.querySelector("#totalTip").style.display = "none";
+document.querySelector("#each").style.display = "none";
 
+function showText(){
+
+	if(BILLAMT.value === "" || SERVICE_FEE.value === 0 || PEOPLE.value === ""){
+		alert("Please enter valid values");
+		return;
+	}else{
+		const serviceFee = (BILLAMT.value * SERVICE_FEE.value);
+		const total = serviceFee + parseInt(BILLAMT.value);
+
+		const totalEach = total / parseInt(PEOPLE.value);
+		console.log(totalEach);
+		TOTAL.innerText = totalEach.toFixed(2);
+
+		document.querySelector("#totalTip").style.display = "block";
+		document.querySelector("#each").style.display = "block";
+	}
 }
 
 CALLBTN.addEventListener("click", showText);
+
